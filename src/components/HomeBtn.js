9ab1dc1e129b4f41c298d300bfddcdef15b79423
Nazar Rohiv <rogivnazariy@gmail.com>
2023-04-25 14:49:00 +0200

    Add HomeBtn styles and functionality

--- src/components/HomeBtn.js
+++ src/components/HomeBtn.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState } from "react";
 
-export const HomeBtn = () => {
-  return (
-    <div>HomeBtn</div>
-  )
+export default function HomeBtn() {
+  const [homebtn, setHomebtn] = useState(false);
+
+  const showButton = () => {
+    if (window.scrollY >= 150) {
+      setHomebtn(true);
+    } else {
+      setHomebtn(false);
+    }
+  };
+
+  const goHome = () => {
+    window.scroll(0, 0);
+  };
+
+  window.addEventListener("scroll", showButton);
+  if (homebtn === true) {
+    return (
+      <button
+        onClick={goHome}
+        className="fixed z-10 bottom-24 right-4 bg-red-700 text-stone-100 text-xs font-bold rounded-lg w-12 h-12"
+      >
+        UP
+      </button>
+    );
+  }
 }
-
-export default HomeBtn
