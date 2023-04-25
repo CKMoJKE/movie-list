7629432da981a2590eaad9447af97d21c3969145
Nazar Rohiv <rogivnazariy@gmail.com>
2023-04-25 10:33:31 +0200

    Add content and styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import TMDB_logo from "../img/logo/tmdb.svg";
+
+const viewTMDB = () => {
+  window.open(`https://www.themoviedb.org/`);
+};
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="max-w-full bg-stone-800">
+      <div className="mx-auto items-center flex justify-between max-w-7xl">
+        <div>
+          <button onClick={viewTMDB}>
+            <img src={TMDB_logo} alt="logo" className="w-48" />
+          </button>
+        </div>
+        <div className="text-stone-200 py-3 flex flex-col">
+          <h1 className="text-lg font-bold">Угоди</h1>
+          <div className="flex gap-5">
+            <a href="/terms" className="text-sm">
+              Умови користування
+            </a>
+            <a href="/" className="text-sm">
+              Політика конфіденційності
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
