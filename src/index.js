9981b1dccde57f4ecc748fccac8c1729bf448eea
Nazar Rohiv <rogivnazariy@gmail.com>
2023-04-25 11:41:05 +0200

    Change routing

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AppProvider } from "./components/context";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <AppProvider>
-      <Router>
+      <BrowserRouter basename='/movie-list'>
         <App />
-      </Router>
+      </BrowserRouter>
     </AppProvider>
   </React.StrictMode>
 );
